e594050f9d5a0b671c5d965d944e77100497f04a
Jaredslocker21 <jaredslocker21@gmail.com>
2022-07-06 11:29:44 +0000

    add f(x) layout verify,update,retrieve

--- run.py
+++ run.py
@@ -31,8 +31,9 @@ def welcome():
     print(colored(('All your selections will be logged with our administration and your therapist will contact you with more information.\n'), 'cyan'))
     
 welcome()
-# love sandwiches example
 
+client_info = []
+# love sandwiches example
 def get_client_info():
     """
     Get clients input data and add it to the google sheet 
@@ -42,36 +43,106 @@ def get_client_info():
     print(f"Your name is {data_str}")
     print(f"Your email is {data_email}")
 
-# #def validate_data(values):
+    
 
-#     print()
+def verify_client_info():
+    """
+    verify name and email is entered correctly letters and email
+    """
+    while True:
+        try:
 
+def update_client_worksheet():
+    """
+    Update client information and append worksheet to a new row
+    """
+
+
+update_client_worksheet()
+verify_client_info()
 get_client_info()
 
-# def therapy_list():
-#     """
-#     retun a list of therapies and definitions for the client to choose
-#     """
-#     print('Select "1" for Occupational Therapy')
-#     print('Select "2" for Sports Therapy')
-#     print('Select "3" for Rehabilitation Therapy')
+def index_offered_therapy():
+    """
+    Function to index available  therapies
+    """
+    titles = therapy_titles():
+    index = 1
+    for title in titles:
+        print(f' {index}. {title}')
+        index += 1
+# Retrieve the different types of therapies from google docs
+def retrieve_therapy():
+    """
+  Display Therapies
+    """
+    print('\n Select the therapy you would like.')
+    print(' Choose the therapy 1,2 or 3.\n')
+
+    index_offered_therapy()
+
+    selection = input('\n Please select a desired therapy numbers 1,2 or 3:\n ')
+
+    if selection == '1':
+        therapy_list('Occupational')
+    elif selection == '2':
+        therapy_list('Sports')
+    elif selection == '3':
+        therapy_list('Rehabilitation')
+    else:
+        print('\n Invalid choice.')
+        print(' You may only choose one of the listed options.\n')
+        return retrieve_therapy()
+
+    print('\n Great Choice!')
+   
+def update_client_worksheet():
+    """
+    Update client information worksheet to append new column - type of therapy 
+    """
+# Choose your therapist, 
+def retrieve_therapist():
+    """
+    Function to display a Therapist,
+    and allow the user to select their therapist.
+    """
+    print('\n Select the therapist you would like.')
+    print(' Choose the therapist 1,2 or 3.\n')
+
+    index_therapist():
+
+    selection = input('\n Please select a therapist numbers 1,2 or 3:\n ')
+
+    if selection == '1':
+        therapy_list('Jared')
+    elif selection == '2':
+        therapy_list('Tor')
+    elif selection == '3':
+        therapy_list('Victoria')
+    else:
+        print('\n Invalid choice.')
+        print(' You may only choose one of the listed options.\n')
+        return retrieve_therapist()
+
+    print('\n Great Choice!')
+   
+
+def index_therapist():
+    """
+    Function to index available  therapies
+    """
+    titles = therapist_name()
+    index = 1
+    for title in titles:
+        print(f' {index}. {title}')
+        index += 1
 
 
-# therapy_list()  
 
 
 
-# def main():
-#     """
-#     Run all program functions
-#     """
-#     data = get_client_data()
-#     client_data = [int(num) for num in data]
-#     update_worksheet(client_data, "client")
-#     new_client_data = match_client_data(client_data)
-#     receipt_for_client = new_client_data   
-#     update_worksheet(stock_data, "stock")
 
-
-
-# main()
+def main()
+    """
+    run all functions
+    """
