715abea56e8ac47bbb7170e6e74d50c7301346ca
Jaredslocker21 <jaredslocker21@gmail.com>
2022-07-07 07:54:07 +0000

    name and check name f(x)is workin

--- run.py
+++ run.py
@@ -4,6 +4,7 @@ Import section
 import os
 import sys
 from termcolor import colored
+from pprint import pprint
 import gspread 
 from google.oauth2.service_account import Credentials
 
@@ -26,8 +27,8 @@ def welcome():
     """
     Introduction to the Application.
     """
-    print(colored(('Welcome to Love Massages.\n'), 'cyan'))
-    print(colored(('Enter your Name and your Email address when prompted below:\n'), 'cyan'))
+    print(colored(('As a current member of love massages.\n'), 'cyan'))
+    print(colored(('Enter your Last Name when prompted below:\n'), 'cyan'))
     print(colored(('Select your type of Massage Therapy \n'), 'cyan'))
     print(colored(('After that select your Massage Therapist.\n'), 'cyan'))
     print(colored(('Your booking will be made and you should see you confirmation.\n'), 'cyan'))
@@ -38,65 +39,73 @@ class Booking:
     def __init__(self, therapy):
         self.customer_name = customer_name
 
-    def set_email(self, email):
-        self.email = email 
-
     def set_therapy(self, therapy):
         self.therapy = therapy
 
     def set_therapist(self, therapist):
         self.therapist = therapist    
 
-def get_customer_name():
-    """ 
-    Get the customer name, validate the the input create the booking
+def name_input():
     """
-    #customer input
-    #validate the input
-    #create a booking
-    booking = Booking(customer_name)
-    return booking
+    The name_input function takes input from the user and stores it 
+    in a variable that can be used further into the program
+    """
+   
+    while True:
+        customer_name = input("PLEASE ENTER A NAME:\n").capitalize()
+        if check_customer_name(customer_name):
+            break
+    print(f"\n Your Booking will be referenced using {customer_name}\n")
+    print(' ')
+    return customer_name
 
-def  get_customer_email():
-    """ 
-    Get the customer email, validate and add it to the booking
-    """
-    #customer input
-    #validate
-    #create booking
-    booking = Booking(customer_email)
 
-def select_therapy(booking):
-    """ 
-    Display types of therapy to the customer, validate and get therapy name
+def check_customer_name(name):
     """
-    #display message
-    #validate therapy 
-    #get therapy name
-    booking.set_therapy(therapy_name)
-    return booking 
+    Funtion checks if the customers name is longer 
+    than 10 or not long enough. 
+    """
+    if len(name) > 10:
+        print('INVALID NAME. Too long')
+        return False
+    elif len(name) == 0:
+        print('Please enter your name')
+    else:
+        return True
+name_input()
+# def select_therapy(booking):
+#     """ 
+#     Display types of therapy to the customer, validate and get therapy name
+#     """
+#     #display message
+#     #validate therapy 
+#     #get therapy name
+#     booking.set_therapy(therapy_name)
+#     return booking 
  
-def select_therapist(booking):
-    # display message
-    # validate the selected therapist
-    # get the therapist name
-    booking.set_therapist(therapist)
-    return booking   
+# def select_therapist(booking):
+#     # display message
+#     # validate the selected therapist
+#     # get the therapist name
+#     booking.set_therapist(therapist)
+#     return booking   
 
-def save_booking(booking):
-    #make google sheet connection
-    #make google sheet call and save the data
+# def save_booking(booking):
+#     #make google sheet connection
+#     #make google sheet call and save the data
 
-def main():
-    booking = get_customer_name()
-    booking = get_customer_email()
-    select_therapy(booking)
-    select_therapist(booking)
-    save_booking(booking)
+# def main():
+#     booking = get_customer_name()
+#     booking = get_customer_phone_number()
+#     select_therapy(booking)
+#     select_therapist(booking)
+#     save_booking(booking)
 
-selection = 1
+# selection = 1
 
-if not selection.isnumeric() or not (int(selection) < Therapies.length and int(selection) > 0)
+# if not selection.isnumeric() or not (int(selection) < Therapies.length and int(selection) > 0)
 
-def getSelectedTherapy(selection):
-    return THERAPIES[selection-1]:
+# def getSelectedTherapy(selection):
+#     return THERAPIES[selection-1]:
+
+# main()
