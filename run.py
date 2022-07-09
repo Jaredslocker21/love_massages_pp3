9154f82069af7af15c6f5c87136cd3506e28b315
Jaredslocker21 <jaredslocker21@gmail.com>
2022-07-09 11:23:15 +0000

    add fx to therapy section

--- run.py
+++ run.py
@@ -59,61 +59,79 @@ def customer_name():
     return customer_name
 
 
-def check_customer_name(name):
+def check_customer_name(customer_name):
     """
     Funtion checks if the customers name is longer 
     than 10 or not long enough. 
     """
-    if len(name) > 20:
+    if len(customer_name) > 20:
         print('INVALID NAME. Too long')
         return False
-    elif len(name) == 0:
+    elif len(customer_name) == 0:
         print('Please enter your name')
     else:
         return True
-customer_name()        
-    # booking = Booking(customer_name)
-    # return booking
-def select_therapy(booking):
-    """ 
-     Display types of therapy to the customer, validate and get therapy name
+    booking = Booking(customer_name)
+    return booking
+customer_name()
+#therapy
+def select_therapy_name():
     """
-#display message
-print("\n Please select the type of Therapy you would like to receive. \n")
-print("Occuptaional Massage Therapy")
-print("Sports Massage Therapy")
-print("Rehabilitation Massage Therapy")
-print(THERAPIES)
-    #validate therapy 
-    #get therapy name
-    booking.set_therapy(therapy_name)
-     return booking 
+    The name_input function takes input from the user and stores it 
+    in a variable that can be used further into the program
+    """
+   
+    while True:
+        therapy_name = input("Please enter therapy:\n").capitalize()
+        if check_therapy_name(therapy_name):
+            break
+    print(f"\n Your have chosen {therapy_name}\n")
+    return therapy_name
+select_therapy_name()
+#     """ 
+#      Display types of therapy to the customer, validate and get therapy name
+#     """
+# #display message
+ #   print("\n Please select the type of Therapy you would like to receive. \n")
+  #  print("Occuptaional Massage Therapy")
+   # print("Sports Massage Therapy")
+    #print("Rehabilitation Massage Therapy")
+    #print(THERAPIES)
+#     #validate therapy 
+#     #get therapy name
+    #booking.set_therapy(therapy_name)
+    #booking = Booking(therapy_name)
+    #return booking
+
+    #booking = Booking(therapy_name)
+    #return booking 
+#select_therapy(booking)
  
-def select_therapist(booking):
-    # display message
-print("Choose your therapist.")
-print(THERAPISTS)
-    # validate the selected therapist
-    # display the therapist name
-    booking.set_therapist(therapist)
-    return booking   
+# def select_therapist(booking):
+#     # display message
+# print("Choose your therapist.")
+# print(THERAPISTS)
+#     # validate the selected therapist
+#     # display the therapist name
+#     booking.set_therapist(therapist)
+#     return booking   
 
-def save_booking(booking):
-    #make google sheet connection
-    #make google sheet call and save the data
+# def save_booking(booking):
+#     #make google sheet connection
+#     #make google sheet call and save the data
 
-def main():
-    booking = get_customer_name()
-    booking = get_customer_phone_number()
-    select_therapy(booking)
-    select_therapist(booking)
-    save_booking(booking)
+# def main():
+#     booking = get_customer_name()
+#     booking = get_customer_phone_number()
+#     select_therapy(booking)
+#     select_therapist(booking)
+#     save_booking(booking)
 
-selection = 1
+# selection = 1
 
-if not selection.isnumeric() or not (int(selection) < Therapies.length and int(selection) > 0)
+# if not selection.isnumeric() or not (int(selection) < Therapies.length and int(selection) > 0)
 
-def getSelectedTherapy(selection):
-    return THERAPIES[selection-1]:
+# def getSelectedTherapy(selection):
+#     return THERAPIES[selection-1]:
 
-main()
+# main()
