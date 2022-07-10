7a2de1dc2248868ea4dfe4a76b1e41261a3098dd
Jaredslocker21 <jaredslocker21@gmail.com>
2022-07-10 08:20:54 +0000

    add fx for therapy/therapists

--- run.py
+++ run.py
@@ -40,11 +40,11 @@ class Booking:
         self.customer_name = customer_name
 
     def set_therapy(self, therapy):
-        self.therapy = therapy
+        self.therapy_name = therapy_name
 
     def set_therapist(self, therapist):
-        self.therapist = therapist    
-
+        self.therapist_name = therapist_name    
+# Customer Name
 def customer_name():
     """
     The name_input function takes input from the user and stores it 
@@ -74,47 +74,69 @@ def check_customer_name(customer_name):
     booking = Booking(customer_name)
     return booking
 customer_name()
-#therapy
+
+#Select your Therapy
 def select_therapy_name():
     """
-    The name_input function takes input from the user and stores it 
+    The select_name_therapy function takes input from the user and stores it 
     in a variable that can be used further into the program
     """
-   
     while True:
+        print("\n Please select the type of Therapy you would like to receive. \n")
+        print(THERAPIES)
+   
         therapy_name = input("Please enter therapy:\n").capitalize()
         if check_therapy_name(therapy_name):
             break
     print(f"\n Your have chosen {therapy_name}\n")
     return therapy_name
-select_therapy_name()
-#     """ 
-#      Display types of therapy to the customer, validate and get therapy name
-#     """
-# #display message
- #   print("\n Please select the type of Therapy you would like to receive. \n")
-  #  print("Occuptaional Massage Therapy")
-   # print("Sports Massage Therapy")
-    #print("Rehabilitation Massage Therapy")
-    #print(THERAPIES)
-#     #validate therapy 
-#     #get therapy name
-    #booking.set_therapy(therapy_name)
-    #booking = Booking(therapy_name)
-    #return booking
 
-    #booking = Booking(therapy_name)
-    #return booking 
-#select_therapy(booking)
- 
-# def select_therapist(booking):
-#     # display message
-# print("Choose your therapist.")
-# print(THERAPISTS)
-#     # validate the selected therapist
-#     # display the therapist name
-#     booking.set_therapist(therapist)
-#     return booking   
+def check_therapy_name(therapy_name):
+    """
+    Check if ther Therapy Chosen is Correct
+    """
+    if len(therapy_name) > 20:
+        print('INVALID NAME. Too long')
+        return False
+    elif len(therapy_name) == 0:
+        print('Please enter your name')
+    else:
+        return True
+    booking = Booking(therapy_name)
+    return booking
+select_therapy_name()
+
+#SelectTherapist
+def select_therapist_name():
+    """
+    The select_name_therapy function takes input from the user and stores it 
+    in a variable that can be used further into the program
+    """
+    while True:
+        print("\n Your Therapist are: \n")        
+        print(THERAPISTS)
+   
+        therapist_name = input("Please select your Therapist by entering ther name:\n").capitalize()
+        if check_therapist_name(therapist_name):
+            break
+    print(f"\n Your have chosen {therapist_name}\n")
+    return therapist_name
+
+def check_therapist_name(therapist_name):
+    """
+    Check if the Therapist Chosen is Correctly
+    """
+    if len(therapist_name) > 20:
+        print('INVALID THERAPIST')
+        return False
+    elif len(therapist_name) == 0:
+        print('Please enter your name')
+    else:
+        return True
+    booking = Booking(therapist_name)
+    return booking
+select_therapist_name()
+
 
 # def save_booking(booking):
 #     #make google sheet connection
