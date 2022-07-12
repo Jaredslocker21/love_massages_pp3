9bfedb5d36fcddec9ad327205e7a1a994af172f4
Jaredslocker21 <jaredslocker21@gmail.com>
2022-07-12 13:51:02 +0000

    add beautification

--- run.py
+++ run.py
@@ -1,11 +1,11 @@
-""" 
+"""
 Import section
 """
 import os
 import sys
 from termcolor import colored
 from pprint import pprint
-import gspread 
+import gspread
 from google.oauth2.service_account import Credentials
 
 SCOPE = [
@@ -21,19 +21,18 @@ SHEET = GSPREAD_CLIENT.open('love_massages_pp3')
 
 THERAPIES = ['Occupational Massage:', 'Sports Massage:', 'Rehabilitation Massage:']
 THERAPISTS = ['Jared:', 'Tor:', 'Victoria:']
-Booking = []
-
 
 def welcome():
+    
     """
     Introduction to the Application.
     """
     print(colored(('As a current member of love massages.\n'), 'cyan'))
-    print(colored(('You are welcome to a free massage through your membership.\n'), 'cyan'))
+    print(colored(('You are welcome to a free massage.\n'), 'cyan'))
     print(colored(('Enter your Name when prompted below:\n'), 'cyan'))
     print(colored(('Select your type of Massage Therapy \n'), 'cyan'))
-    print(colored(('After that select your Massage Therapist.\n'), 'cyan'))
-    print(colored(('Your booking will be made and you should see you confirmation.\n'), 'cyan'))
+    print(colored(('After that select your Therapist.\n'), 'cyan'))
+    print(colored(('Your booking will be made.\n'), 'cyan'))
 
 welcome()
 
@@ -45,22 +44,23 @@ class Booking:
         self.therapy_name = select_therapy_name
 
     def select_therapist_name(self, select_therapist_name):
-        self.therapist_name = select_therapist_name   
-    
+        self.therapist_name = select_therapist_name  
+   
     def get_booking_data(self):    
-        return [self.customer_name, self.therapy_name, self.therapist_name] 
- 
+        return [self.customer_name, self.therapy_name, self.therapist_name]
+
 def select_customer_name():
+
     """
     The name_input function takes input from the user and stores it 
     in a variable that can be used further into the program
-    """
-   
+    """  
     while True:
         customer_name = input("Please enter your name for a booking:\n").capitalize()
         if check_customer_name(customer_name):
             break
-    print(f"\n Your Booking will be referenced using {customer_name}\n")
+    print (colored((f"\n Your Booking will be referenced using\
+    {customer_name}\n"),"magenta"))
     booking = Booking(customer_name)
     return booking
     
@@ -82,16 +82,15 @@ def select_therapy_name(booking):
     """
     The select_name_therapy function takes input from the user and stores it 
     in a variable to be called by booking
-    """
-    
+    """   
     while True:
-        print("\n Please select the type of Therapy you would like to receive. \n")
+        print("\n Please select the type of Therapy.\n")
         print(*THERAPIES, sep='\n')
    
         therapy_name = input("Please enter therapy:\n").capitalize()
         if check_therapy_name(therapy_name):
             break
-    print(f"\n You have chosen {therapy_name}\n")
+    print(colored((f"\n You have chosen {therapy_name}\n"),"magenta"))
     booking.select_therapy_name(therapy_name)
     return booking
     
@@ -110,18 +109,18 @@ def check_therapy_name(name):
 #Select Therapist
 def select_therapist_name(booking):
     """
-    The select_name_therapist function takes input from the user and stores it 
+    The select_name_therapist function takes 
+    input from the user and stores it 
     as a variable( to be called later to print booking
     """
     while True:
-        print("\n Your Therapist are: \n")        
+        print("\n Your Therapist's are: \n")
         print(*THERAPISTS, sep='\n')
-        
-   
-        therapist_name = input("Please select your Therapist by entering their name:\n").capitalize()
+          
+        therapist_name = input("Please select your Therapist name:\n").capitalize()
         if check_therapist_name(therapist_name):
             break
-    print(f"\n You have chosen {therapist_name}\n")
+    print(colored((f"\n You have chosen {therapist_name}\n"),"magenta"))
     booking.select_therapist_name(therapist_name)
     return booking
 
@@ -142,11 +141,12 @@ def update_worksheet(data, worksheet):
     Receives list of intergers to be inserted into a worksheet
     Update the relevant worksheet with the data provided
         """
-    
-    print(f"Updating {worksheet} worksheet....\n")
+    print(colored((f"Updating our {worksheet} is in progress....\n"),"magenta"))
     worksheet_to_update = SHEET.worksheet(worksheet)
     worksheet_to_update.append_row(data.get_booking_data())
-    print(f"{worksheet} worksheet updated successfully\n")
+    print(colored(("Thank you for choosing Love Massages!\n"),"magenta"))
+    print(colored(("Your booking has been updated!\n"),"magenta"))
+    print(colored(("Your therapist will be contacting you shortly\n"),"magenta"))
 
 def main():
     
@@ -154,7 +154,4 @@ def main():
     select_therapy_name(booking)
     select_therapist_name(booking)
     update_worksheet(booking, "bookings")
-    
-    print(booking)
-    
 main()
